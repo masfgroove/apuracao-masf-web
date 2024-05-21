a91120ec757b4a2d3a7657c25561634fb9b421e2
masfgroove <masfgroove2012@gmail.com>
2024-05-20 23:19:43 -0300

    first commit

--- src/api/app.py
+++ src/api/app.py
@@ -4,7 +4,7 @@ from config import email, senha
 
 app = Flask(__name__)
 mail = Mail(app)
-app.secret_key = 'zani19'
+app.secret_key = 'Ma@010246'
 
 mail_settings = {
     "MAIL_SERVER":'smtp.gmail.com',
@@ -36,9 +36,23 @@ def sobre():
 def projetos():
     return render_template('/public/projetos.html')
 
+@app.route('/java')
+def java():
+    return render_template('/public/java.html')
+
+
 @app.route('/contato')
 def contato():
     return render_template('/public/contato.html')
+
+@app.route('/imagemp1')
+def imagemp1():
+    return render_template('/public/imagemp1.html')
+
+@app.route('/imagemp2')
+def imagemp2():
+    return render_template('/public/imagemp2.html')
+
 
 @app.route('/send', methods=['GET', 'POST'])
 def send():
@@ -50,9 +64,9 @@ def send():
         )
 
         msg = Message(
-            subject = f'{formContato.nome} te enviou uma mensagem no portfólio',
+            subject = f'{formContato.nome} te enviou uma mensagem no portfÃ³lio',
             sender = app.config.get("MAIL_USERNAME"),
-            recipients = ['thiago.zani@uol.com.br', app.config.get("MAIL_USERNAME")],
+            recipients = ['marcoantonio639639@gmail.com', app.config.get("MAIL_USERNAME")],
             body = f'''
 
             {formContato.nome} com o e-mail {formContato.email}, te enviou a seguinte mensagem:
